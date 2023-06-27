1d8b8477b75e0043bd7000c3c43831912cda284b
Olga Lebedeva <olga2001lebedeva@yandex.ru>
2023-06-28 02:27:06 +0500

    Автотесты после ревью

--- helpers/status.js
+++ helpers/status.js
@@ -1,9 +1,11 @@
-export enum HttpStatus {
-  Success = 200,
-  BadRequest = 400,
-  Unauthorized = 401,
-  Forbidden = 403,
-  NotFound=404,
-  NotAllowed = 405,
-  InternalError = 500,
-}
+const HttpStatus = {
+  Success: 200,
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  NotFound: 404,
+  NotAllowed: 405,
+  InternalError: 500,
+};
+
+module.exports = HttpStatus;
